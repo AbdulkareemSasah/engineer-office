7258047fe379354809635c2a91b3c5f6f557c2d4
abdulkareemsasah <abdulkareemsasah@gmail.com>
2023-11-17 15:54:00 +0300

    database

--- EngineerOffice/settings.py
+++ EngineerOffice/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +87,12 @@ WSGI_APPLICATION = "EngineerOffice.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DATABASE_NAME"),                      
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("PW"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PORT':os.environ.get("DATABASE_PORT"),
     }
 }
 
